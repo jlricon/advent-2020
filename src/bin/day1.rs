9ddc5c5c0e5403281b102d94670b078ad73401b5
Jose Luis Ricon <artirj@gmail.com>
2020-12-02 10:40:34 -0800

    make nice

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,23 +1,26 @@
+use itertools::Itertools;
 use std::collections::HashMap;
-
 const SUM_UP_TO: i32 = 2020;
+
 fn part1(nums: &Vec<i32>) {
-    for (first, val1) in nums.iter().enumerate() {
-        for val2 in nums.iter().skip(first) {
+    nums.into_iter()
+        .tuple_combinations()
+        .for_each(|(val1, val2)| {
             if val1 + val2 == SUM_UP_TO {
                 println!("{}", val1 * val2);
                 return;
             }
-        }
-    }
+        })
 }
+
 fn part2(nums: &Vec<i32>) {
     let mut hash = HashMap::<i32, (&i32, &i32)>::new();
-    for (first, val1) in nums.iter().enumerate() {
-        for val2 in nums.iter().skip(first) {
+    nums.into_iter()
+        .tuple_combinations()
+        .for_each(|(val1, val2)| {
             hash.entry(val1 + val2).or_insert((val1, val2));
-        }
-    }
+        });
+
     for val in nums.iter() {
         let remainder = SUM_UP_TO - val;
         if let Some(ent) = hash.get(&remainder) {
@@ -32,5 +35,5 @@ fn main() {
         .map(|n| n.parse::<i32>().unwrap())
         .collect();
     part1(&nums);
-    part2(&nums)
+    part2(&nums);
 }

192eeeea3a29ca5f509946b56c258a50bd074e27
Jose Luis Ricon <artirj@gmail.com>
2020-12-02 13:22:05 -0800

    bench

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,3 +1,4 @@
+use easybench::bench;
 use itertools::Itertools;
 use std::collections::HashMap;
 const SUM_UP_TO: i32 = 2020;
@@ -12,23 +13,37 @@ fn part1(nums: &Vec<i32>) {
             }
         })
 }
+fn part2_tuples(nums: &Vec<i32>) -> i32 {
+    let mut ret = 0;
+    for (val1, val2, val3) in nums.into_iter().tuple_combinations() {
+        if val1 + val2 + val3 == SUM_UP_TO {
+            //println!("{}", val1 * val2 * val3);
+            ret = val1 * val2 * val3;
+            break;
+        }
+    }
+    ret
+}
 
-fn part2(nums: &Vec<i32>) {
+fn part2(nums: &Vec<i32>) -> i32 {
     let mut hash = HashMap::<i32, (&i32, &i32)>::new();
     nums.into_iter()
         .tuple_combinations()
         .for_each(|(val1, val2)| {
             hash.entry(val1 + val2).or_insert((val1, val2));
         });
-
+    let mut ret = 0;
     for val in nums.iter() {
         let remainder = SUM_UP_TO - val;
         if let Some(ent) = hash.get(&remainder) {
-            println!("{}", ent.0 * ent.1 * val);
-            return;
+            //println!("{}", ent.0 * ent.1 * val);
+            ret = ent.0 * ent.1 * val;
+            break;
         }
     }
+    ret
 }
+
 fn main() {
     let nums: Vec<i32> = include_str!("../../input/day1.txt")
         .split("\n")
@@ -36,4 +51,5 @@ fn main() {
         .collect();
     part1(&nums);
     part2(&nums);
+    part2_tuples(&nums);
 }
